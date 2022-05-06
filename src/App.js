c0b7cd19c016587d111e62e978fa812231c7eebb
Neemahj~ <neemahjinadu@gmail.com~>
2022-05-06 18:21:38 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
 
 function App() {
+  const [showMenu, setShowMenu] = useState(false)
+  const handleShowMenu=()=>{
+    setShowMenu(!showMenu)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${showMenu?"active":" "}`}>
+      <div className="info">
+        <img
+          src="https://thumbs.dreamstime.com/b/animation-portrait-young-black-woman-dreadlocks-animation-portrait-young-black-woman-dreadlocks-background-179379079.jpg"
+          alt="dreed"
+        />
+        <h1>Neemah</h1>
+      </div>
+
+      <div className="hamburger" onClick={handleShowMenu}>
+        <div className="each-ham"></div>
+        <div className="each-ham"></div>
+        <div className="each-ham"></div>
+      </div>
+      
+      <ul className="items" >
+        <li>Home</li>
+        <li>Contact Us</li>
+        <li>Services</li>
+        <li>About Us</li>
+      </ul>
     </div>
   );
 }
+
 
 export default App;
