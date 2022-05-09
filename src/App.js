9311f8e27fe15ba916fb8455f5dc1020d094e7e5
Neemahj~ <neemahjinadu@gmail.com~>
2022-05-09 16:42:48 +0100

    Fixed review updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import './App.css';
-import { useState } from 'react';
-
-
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [showMenu, setShowMenu] = useState(false)
-  const handleShowMenu=()=>{
-    setShowMenu(!showMenu)
-  }
+  const [showMenu, setShowMenu] = useState(false);
+  const handleShowMenu = () => {
+    setShowMenu(!showMenu);
+  };
   return (
-    <div className={`App ${showMenu?"active":" "}`}>
+    <header className={`App ${showMenu ? "active" : " "}`}>
       <div className="info">
         <img
           src="https://thumbs.dreamstime.com/b/animation-portrait-young-black-woman-dreadlocks-animation-portrait-young-black-woman-dreadlocks-background-179379079.jpg"
@@ -23,16 +21,23 @@ function App() {
         <div className="each-ham ham2"></div>
         <div className="each-ham ham3"></div>
       </div>
-      
-      <ul className="items" >
-        <li><a href='#home'>Home</a></li>
-        <li><a href='#contact'>Contact Us</a></li>
-        <li><a href='#services'>Services</a></li>
-        <li><a href='#about'>About Us</a></li>
+
+      <ul className="items">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#contact">Contact Us</a> 
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#about">About Us</a>
+        </li>
       </ul>
-    </div>
+    </header>
   );
 }
-
 
 export default App;
